9eb221a7be590ee44de0713dd561d4a96d20b915
jenikamban <nenijeni275@gmail.com>
2026-04-23 06:14:10 -0700

    feat: penyiapan blade component untuk layout utama

---
+++ resources/views/produk-tas/create.blade.php
@@ -0,0 +1,7 @@
+<x-app>
+
+    <x-slot:title> {{ $title }}</x-slot>
+
+    <h1 class="fw-bold">Form create produk tas</h1>
+
+</x-app>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('produk-tas', function () {
+    return view('produk-tas.index', ['title' => 'tas']);
+});
+
+Route::get('produk-tas/create', function () {
+    return view('produk-tas.create', ['title' => ' create tas']);
+});
